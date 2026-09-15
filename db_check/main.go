e51b2e0ee6883d38ae1cbb662d3d065d99ef597a
iankarungaru <karungaruian@gmail.com>
2026-09-15 12:37:40 +0300

    Install Postgres, create patient_db, confirm Go connectivity via pgx

---
+++ db_check/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	connStr := "postgres://postgres:mamayogho@localhost:5432/patient_db"
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+	}
+	defer conn.Close(context.Background())
+
+	fmt.Println("Successfully connected to patient_db!")
+}
